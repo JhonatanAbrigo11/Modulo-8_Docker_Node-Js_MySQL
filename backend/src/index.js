9ece32ed442011dee5fa6ad6dc77e37dd6905990
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2025-09-18 17:58:46 -0500

    Archivo .env.test

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,20 @@
+ require('dotenv').config(); 
+
 const express = require('express')
-const app = express();
+const app = express(); 
+const port = process.env.PORT || 3000;
+const jwtSecret= process.env.JWT_SECRET;
 
 app.use(express.json());
 
+console.log("clave secreta",jwtSecret)
+console.log("port",port)
 
 app.get('/',(req,res)=> {
 	res.send('API backend funcionando');
 });
 
-const PORT = process.env.POR || 3000;
-app.listen(PORT, () => {
-	console.log(`Servidor escuchando en el puerto ${PORT}`);
+
+app.listen(port, () => {
+	console.log(`Servidor escuchando en el puerto ${port}`);
 });
