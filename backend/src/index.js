f0971efd65df97b617b4ddae1fe069131756d996
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2025-09-26 22:55:35 -0500

    Api documentada con Scalar

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,56 @@
-require('dotenv').config(); 
-const tareasRoutes = require ("./routes/tareaRoutes")
-const authRouter= require('./routes/authRouter')
+require('dotenv').config();
+const tareasRoutes = require("./routes/tareaRoutes")
+const authRouter = require('./routes/authRouter')
 const express = require('express')
-const app = express(); 
+const app = express();
 const port = process.env.PORT || 3000;
-const jwtSecret= process.env.JWT_SECRET;
-
+const jwtSecret = process.env.JWT_SECRET;
+const path = require('path');
 const cors = require('cors')
+
+const { apiReference } = require('@scalar/express-api-reference');
+
 const corsOptions = {
-	origin:'http://localhost:3000' || process.env.CORS_ORIGIN,
-	
+	origin: 'http://localhost:3000' || process.env.CORS_ORIGIN,
+	credentials: true
+
 }
 app.use(express.json());
 
-console.log("clave secreta",jwtSecret)
-console.log("port",port)
+console.log("clave secreta", jwtSecret)
+console.log("port", port)
 
-app.get('/',(req,res)=> {
+app.get('/', (req, res) => {
 	res.send('API backend funcionando');
 });
 
-app.use('/api', tareasRoutes); 
-app.use('/api',authRouter)
+app.use('/api', tareasRoutes);
+app.use('/api', authRouter)
 
+app.use('/docs', apiReference({
+	theme: 'purple',
+	layout: 'modern',
+	spec: {
+		url: '/api/openapi.yaml',
+	},
+	configuration: {
+		showSidebar: true,
+		hideDownloadButton: false,
+		hideTryItPanel: false,
+		authentication: {
+			preferredSecurityScheme: 'bearerAuth',
+			apiKey: {
+				token: 'token'
+			}
+		}
+	}
+}));
 
+app.get('/api/openapi.yaml',(req,res)=>{
+	res.setHeader('Content-Type','application/x-yaml')
+	res.sendFile(path.join(__dirname, '../docs/openapi.yaml'))
+})
 app.listen(port, () => {
 	console.log(`Servidor escuchando en el puerto ${port}`);
+	console.log(`Docuemntacion disponible en: http://localhost:${port}/docs`)
 });
